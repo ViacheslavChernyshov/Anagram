48342482199298f497bdf1d2b5a121beb94f9ef7
Oleg Pitsik <slimmax002@mail.ru>
2019-07-01 21:46:12 +0600

    Reverse with for

--- src/com/olpi/anagram/Anagram.java
+++ src/com/olpi/anagram/Anagram.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 
 public class Anagram {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter \"exit\" to close the program. \nWrite a text:");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter \"exit\" to close the program. \nWrite a text:");
 
-		while (scan.hasNext()) {
-			String initialText = scan.next();
+        while (scan.hasNext()) {
+            String initialText = scan.nextLine();
 
-			if (initialText.equalsIgnoreCase("exit")) {
-				break;
-			}
-			Reverse reverse = new Reverse();
-			String finishText = reverse.reverseText(initialText);
-			System.out.println(finishText + " ");
-		}
-		scan.close();
-	}
+            if (initialText.equalsIgnoreCase("exit")) {
+                break;
+            }
+            Reverse reverse = new Reverse();
+            String finishText = reverse.reverseText(initialText);
+            System.out.println(finishText);
+        }
+        scan.close();
+    }
 }
--- src/com/olpi/anagram/Reverse.java
+++ src/com/olpi/anagram/Reverse.java
@@ -2,35 +2,50 @@ package com.olpi.anagram;
 
 public class Reverse {
 
-	public String reverseText(String initialText) throws NullPointerException {
-		if (initialText == null) {
-			throw new NullPointerException("String is null");
-		}
-		return reverseOnlyLetters(initialText);
-	}
+    public String reverseText(String initialText) throws NullPointerException {
+        if (initialText == null) {
+            throw new NullPointerException("String is null");
+        }
+        return reverseOnlyLetters(initialText);
+    }
 
-	private String reverseOnlyLetters(String initialText) {
-		StringBuilder finalText = new StringBuilder(initialText);
+    private String reverseOnlyLetters(String text) {
 
-		int firstIndex = 0;
-		int secondIndex = initialText.length() - 1;
+        StringBuilder finalText = new StringBuilder();
+        StringBuilder endText = new StringBuilder();
 
-		while (secondIndex >= 0 && firstIndex <= initialText.length() - 1) {
+        for (int x = 0; x < text.length(); x++) {
+            finalText.append(text.charAt(x));
 
-			while (!Character.isLetter(initialText.charAt(firstIndex)) && firstIndex < initialText.length() - 1) {
-				firstIndex++;
-			}
-			while (!Character.isLetter(finalText.charAt(secondIndex)) && secondIndex > 0) {
-				secondIndex--;
-			}
-			if (Character.isLetter(finalText.charAt(secondIndex))
-					&& Character.isLetter(initialText.charAt(firstIndex))) {
-				finalText.setCharAt(secondIndex, initialText.charAt(firstIndex));
-			}
-			firstIndex++;
-			secondIndex--;
+            if (text.charAt(x) == ' ' || x == text.length() - 1) {
+                String initialText = finalText.toString();
 
-		}
-		return finalText.toString();
-	}
+                for (int firstIndex = 0, secondIndex = initialText.length() - 1; firstIndex <= initialText.length() - 1
+                        || secondIndex >= 0; firstIndex++, secondIndex--) {
+
+                        for (int i = firstIndex; !Character.isLetter(initialText.charAt(i))
+                                && i < initialText.length() - 1; i++) {
+                            firstIndex++;
+                            System.out.println("firstIndex=" + firstIndex);
+                        }
+                  
+                        for (int i = secondIndex; !Character.isLetter(finalText.charAt(i)) && i < 0; i--) {
+                            System.out.println("secondIndex=" + secondIndex);
+                            secondIndex--;
+                        }
+
+                        if (Character.isLetter(initialText.charAt(firstIndex))
+                                && Character.isLetter(finalText.charAt(secondIndex))) {
+                            finalText.setCharAt(secondIndex, initialText.charAt(firstIndex));
+                        }
+                    }
+
+                }
+                endText.append(finalText.toString());
+                finalText.setLength(0);
+            }
+        
+        return endText.toString();
+    }
+
 }
